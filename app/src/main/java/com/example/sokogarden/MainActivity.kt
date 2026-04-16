e9b52b795c4eeae1ba3ee41516bef00c7ed581e7
Isaac-Maloba <isaaccmaloba@gmail.com>
2026-04-16 07:05:23 +0300

    updated code

--- app/src/main/java/com/example/sokogarden/MainActivity.kt
+++ app/src/main/java/com/example/sokogarden/MainActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -80,5 +82,18 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+
+//        Find the RecylerView and progress bar by use of their IDs
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val progressbar = findViewById<ProgressBar>(R.id.progressbar)
+
+//        Specify the API url endpoint for fetching the products
+        val url = "https://maloba.alwaysdata.net/android/api/get_products"
+
+//        Import the helper class
+        val helper = ApiHelper(applicationContext)
+
+//        Inside of the helper class, access the function loadProducts
+        helper.loadProducts(url, recyclerView, progressbar)
     }
 }
